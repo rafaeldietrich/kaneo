4f758e90bee6d697cb36341f9b9d65cc75ce630e
rafaeldietrich <contato@rafaeldietrich.com>
2025-11-03 17:12:25 -0300

    volta explicita a liberação

--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -35,10 +35,10 @@ const isCrossSubdomain = (() => {
 })();
 
 export const auth = betterAuth({
-  // baseURL: process.env.KANEO_API_URL || "http://kaneo-frontend:1337" || "https://tasks-api.facta.dietrichconsultoria.com.br/",
-  //trustedOrigins: [process.env.KANEO_CLIENT_URL || "http://localhost:5173" || "https://tasks.facta.dietrichconsultoria.com.br/"],
-  baseURL: apiUrl,
-  trustedOrigins: [clientUrl],
+  baseURL: process.env.KANEO_API_URL || "http://kaneo-frontend:1337" || "https://tasks-api.facta.dietrichconsultoria.com.br/",
+  trustedOrigins: [process.env.KANEO_CLIENT_URL || "http://localhost:5173" || "https://tasks.facta.dietrichconsultoria.com.br/"],
+  // baseURL: apiUrl,
+  // trustedOrigins: [clientUrl],
   secret: process.env.AUTH_SECRET || "",
   basePath: "/api/auth",
   database: drizzleAdapter(db, {
